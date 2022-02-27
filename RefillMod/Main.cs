8e5faefe52c5acfd9454614b9ad5db84bfcb765b
dani0105 <danielrojas0105@gmail.com>
2022-02-26 22:07:53 -0600

    Initial Commit

---
+++ RefillMod/Main.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using ModLoader;
+using SFS;
+using SFS.Parts;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RefillMod
+{
+    public class Main : SFSMod
+    {
+        public static GameObject menuObject;
+
+        public static Harmony patcher;
+        public string getModAuthor()
+        {
+            return "Dani0105";
+        }
+
+        public string getModName()
+        {
+            return "ReFill";
+        }
+
+        public void load()
+        {
+            Main.patcher = new Harmony("website.danielrojas.RefillMod");
+            Main.patcher.PatchAll();
+
+            Loader.modLoader.suscribeOnChangeScene(this.OnSceneLoaded);
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if(scene.name == "World_PC")
+            {
+                GameObject menu = new GameObject("RefillMod");
+                menu.AddComponent<Menu>();
+                UnityEngine.Object.DontDestroyOnLoad(menu);
+                menu.SetActive(true);
+                Main.menuObject = menu;
+            }
+            else
+            {
+                UnityEngine.Object.Destroy(menuObject);
+            }
+        }
+
+        public void unload()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ RefillMod/Menu.cs
@@ -0,0 +1,34 @@
+﻿
+using SFS.Parts.Modules;
+using SFS.World;
+using UnityEngine;
+
+namespace RefillMod
+{
+    class Menu : MonoBehaviour
+    {
+        public static Rocket playerRocket;
+
+        public Rect windowRect = new Rect((float)Screen.width * 0.05f, (float)Screen.height * 0.05f, 200f, 200f);
+
+        public void OnGUI()
+        {
+            windowRect = GUI.Window(0, windowRect, new GUI.WindowFunction(this.windowFunc), "Menu");
+        }
+
+        public void windowFunc(int windowID)
+        {
+            if (GUI.Button(new Rect(10f, 20f, 190f, 20f), " Refill tanks"))
+            {
+
+                foreach (ResourceModule tank in Menu.playerRocket.partHolder.GetModules<ResourceModule>())
+                {
+                    tank.AddResource(1000f);
+                }
+               
+            }
+        }
+
+    }
+
+}
